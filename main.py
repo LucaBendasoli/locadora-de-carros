bb232577105e9e9fa8d71f7f35e88fb10c77d71d
LucaAgostinho <lucaabenda@icloud.com>
2025-01-20 17:10:01 -0300

    refactor: adicionei identacao com pylint

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-# Projeto Locadora de Carros
-from utils import *
+"""Projeto Locadora de Carros"""
+# Autor: Luca Bendasoli
+
+from utils import limpar_terminal, escolha_invalida, escolhe_carro, carros_disponiveis
 
 def inicio():
+    """Essa função chama a tela inicial do programa"""
     print("Bem vindo a Locadora de Carros!")
     print("O que deseja fazer?")
     print("0 - Mostrar Porifólio | 1 - Alugar um carro | 2 - Devolver um carro" )
 
 def escolhe_opcao() -> None:
+    """Pede que o usuário escolha uma opção e chama a função correspondente"""
     escolha = input()
     try:
         if int(escolha) == 0:
@@ -16,30 +20,39 @@ def escolhe_opcao() -> None:
         elif int(escolha) == 2:
             devolver_um_carro()
         else:
-            escolha_invalida()  
+            escolha_invalida()
     except:
         escolha_invalida()
-        
+
 def mostrar_portifolio(main_func: bool = False) -> None:
+    """Percorre um dicionário do portifolio e imprime os carros disponíveis
+    
+    Args:
+        main_func (bool, optional): Se a função está
+        sendo chamada pelo menu principal. Padrão é False."""
     limpar_terminal()
     for indice, (carro, valor) in enumerate(carros_disponiveis.items()):
         print(f"[{indice}] {carro} - {valor}")
     if main_func:
         input("Pressione enter para voltar")
         limpar_terminal()
-        
+
 def alugar_um_carro() -> None:
+    """Aluga um carro para o usuário e remove o carro
+    do portifólio e adicona aos carros alugados."""
     limpar_terminal()
     mostrar_portifolio()
     print("Escolha o código do carro")
     escolhe_carro()
-    
+
 def devolver_um_carro() -> None:
+    """Devolve um carro para o portifólio de carros 
+    disponíveis e remove dos carros alugados."""
     limpar_terminal()
     print("Carro devolvido")
     input("Pressione enter para voltar")
     limpar_terminal()
-    
+
 if __name__ == '__main__':
     while True:
         inicio()
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+"""Este arquivo contém funções e variáveis utilitárias para o programa principal"""
+
 import os
 
 carros_disponiveis = {
@@ -12,38 +14,38 @@ carros_disponiveis = {
 }
 
 def escolhe_carro() -> None:
+    """Essa função permite que o usuário escolha um carro para alugar.
+    A função irá validar a escolha do usuário e responder a sua solicitação."""
     escolha = input()
     try:
         escolha = int(escolha)
         if escolha < 0 or escolha >= len(carros_disponiveis):
             escolha_invalida()
-        
         dias = int(input("Quantos dias você quer alugar este carro?\n"))
         chave = list(carros_disponiveis.keys())[escolha]
         valor_por_dia = list(carros_disponiveis.values())[escolha].split()[1]
         valor = int(valor_por_dia.split("/")[0])
-        
-        desistencia = input(f"Você escolheu {chave} por {dias} dias.\nO aluguel totalizaria R$ {valor * dias}. Deseja alugar?\n\n0 - SIM | 1 - NÃO\n") 
-               
+        print(f"Você escolheu {chave} por {dias} dias.")
+        print(f"O aluguel totalizaria R$ {valor * dias}.")
+        print("Deseja alugar?")
+        desistencia = input("0 - SIM | 1 - NÃO\n")
         if desistencia not in ['0', '1']:
             escolha_invalida()
             return
-        elif desistencia == '1':
+        if desistencia == '1':
             limpar_terminal()
             return
-              
         carros_disponiveis.pop(chave, None)
         limpar_terminal()
         print(f"Parabéns você alugou o {chave} por {dias} dias.")
-    except Exception as e:
-        print(e)
+    except:
         escolha_invalida()
 
 def escolha_invalida():
+    """Essa função é chamada quando o usuário faz uma escolha inválida."""
     limpar_terminal()
     print("Escolha inválida, tente novamente")
-        
 
 def limpar_terminal() -> None:
+    """Essa função limpa o terminal."""
     os.system('cls' if os.name == 'nt' else 'clear')
-    
